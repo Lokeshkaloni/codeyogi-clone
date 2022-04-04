161de26ad3cefbef4d52de74b9084441f18ad5be
lokesh kaloni <lokeshkaloni@gmail.com>
2022-04-04 17:53:30 +0530

    added logout button

--- src/Header.jsx
+++ src/Header.jsx
@@ -5,9 +5,13 @@ function Header(){
     return(
         <div className= "bg-gray-800 h-16 rounded-md flex w-full items-center justify-between">
             <h1 className= "text-white ml-6 p-2">CODEYOGI | Welcome , Lokesh</h1>
+
+            <div>
+                <button className= "border px-4 py-2 bg-indigo-600 hover:bg-indigo-300">logout</button>
             <Link className="text-white p-2 border hover:bg-gray-600 rounded-md mr-2" to="/Lectures">
 					go back to Lectures
 				</Link>
+                </div>
         </div>
     )
 }
