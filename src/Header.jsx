eecf13a7dff614acd31a5b5ee52bdad2929b6f47
lokesh kaloni <lokeshkaloni@gmail.com>
2022-04-04 17:57:48 +0530

    modify logout button

--- src/Header.jsx
+++ src/Header.jsx
@@ -6,8 +6,8 @@ function Header(){
         <div className= "bg-gray-800 h-16 rounded-md flex w-full items-center justify-between">
             <h1 className= "text-white ml-6 p-2">CODEYOGI | Welcome , Lokesh</h1>
 
-            <div>
-                <button className= "border px-4 py-2 bg-indigo-600 hover:bg-indigo-300">logout</button>
+            <div className= "space-x-2">
+                <button className= "border px-4 py-2 rounded-md text-white bg-indigo-600 hover:bg-indigo-300">logout</button>
             <Link className="text-white p-2 border hover:bg-gray-600 rounded-md mr-2" to="/Lectures">
 					go back to Lectures
 				</Link>
