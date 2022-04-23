4a28eda2bdc2e119f4e500389a8cd87f023bcb7b
lokesh kaloni <lokeshkaloni@gmail.com>
2022-04-23 21:04:45 +0530

    some corrections

--- src/ProfilePage.jsx
+++ src/ProfilePage.jsx
@@ -6,7 +6,7 @@ function ProfilePage() {
   return (
     <div className="w-full flex flex-col justify-center">
       <Header />
-      <div className="border px-4 py-8 border-gray-500 h-screen rounded-md mt-1 w-full">
+      <div className="border px-4 py-8 border-gray-500 rounded-md mt-1 w-full">
         <h1 className="text-3xl font-bold text-indigo-700">profile view</h1>
         <div className="mt-4 space-y-2">
           <Profile></Profile>
